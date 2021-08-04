73bc75ace026b739275c74e24cc045352783b410
PythonicNoob <atulsyan850@gmail.com>
2021-08-04 11:10:08 +0530

    Changed model, generates license automatically

--- authServer/settings.py
+++ authServer/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
--- licensekeys/models.py
+++ licensekeys/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+import random
+import string
+
+def generate_license():
+    keyset = string.ascii_uppercase + string.digits
+    keysetpart = lambda : "".join(random.choices(keyset, k=3))
+    return "-".join([keysetpart() for _ in range(4)])
 
 # Create your models here.
 class LicenseKey(models.Model):
-    key = models.CharField(max_length=15, unique=True)
+    key = models.CharField(max_length=15, default=generate_license, editable=False, primary_key=True)
     hardware_id = models.CharField(max_length=200, blank=True)
     name = models.CharField(max_length=255)
-    email = models.EmailField(primary_key=True)
-    last_activity = models.DateTimeField(blank=True)
-    active = models.BooleanField(default=True)
+    email = models.EmailField(null=True, blank=True)
+    last_activity = models.DateTimeField(blank=True, null=True)
+    active = models.BooleanField(default=True) #If the license key is active or has expired
